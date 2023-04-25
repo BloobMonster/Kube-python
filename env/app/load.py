949eeedc3b13cf5062e74db31ef318b9e3f211ed
cawfeecoder <nick@frushco.de>
2023-04-24 21:59:45 -0400

    fixup load script

--- env/app/load.py
+++ env/app/load.py
@@ -1,18 +1,18 @@
 import csv
 import datetime
 
-from app import models
-from app.database import SessionLocal, engine
+from database.Database import SessionLocal, engine
+from model.Models import Record, Base
 
 db = SessionLocal()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 with open("sars_2003_complete_dataset_clean.csv", "r") as f:
     csv_reader = csv.DictReader(f)
 
     for row in csv_reader:
-        db_record = models.Record(
+        db_record = Record(
             date=datetime.datetime.strptime(row["date"], "%Y-%m-%d"),
             country=row["country"],
             cases=row["cases"],
